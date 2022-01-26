42ad725733b1e569d1fdc0298de98260a3bfd848
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-26 14:44:35 +0800

    semi complete, stage 2

---
+++ SistemSarangLebah_FPW/Parent_Class/Lebah.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemSarangLebah_FPW
+{
+    class Lebah
+    {
+        // private string Job { get; set; }
+        private readonly string Job = "";
+
+        protected readonly float HargaPerShift = 0f;
+
+        //---------------------------------------------------------
+        //---------------------------------------------------------
+        //---------------------------------------------------------
+        public void KerjaNextShift()
+        {
+
+        }
+
+        protected virtual void DoJob()
+        {
+
+        }
+
+
+
+
+
+    }
+}
---
+++ SistemSarangLebah_FPW/Worker_Classes/ProduserMadu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemSarangLebah_FPW
+{
+    class ProduserMadu : Lebah
+    {
+        //override ﬂoat CostPerShift
+        //protected virtual float HargaPerShift = 0f;
+        private readonly float HargaPerShift = 1.0f;
+        
+        
+        
+        //protected virtual void DoJob()
+        protected override void DoJob()
+        {
+            base.DoJob();
+        }
+
+    }
+}
